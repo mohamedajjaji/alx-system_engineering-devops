9c5638f1b00d3d81aa87921877c5a6a7e31ebf33
Mohamed AJJAJI <majjaji98@gmail.com>
2024-01-16 16:30:59 -0800

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,17 @@ def number_of_subscribers(subreddit):
     """ Return the total number of subscribers on a given subreddit """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    try:
+        if response.status_code != 200:
+            print("Error: status code {}".format(response.status_code))
+            return 0
+        else:
+            results = response.json().get("data")
+            return results.get("subscribers")
+    except ValueError as e:
+        print("Error decoding JSON:", e)
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
